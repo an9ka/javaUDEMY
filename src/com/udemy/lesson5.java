8a54c7ac80eff0010888c4ca546f1c837d825961
an9ka <you@example.com>
2024-12-06 00:09:25 +0300

    вызов метода

--- src/com/udemy/lesson5.java
+++ src/com/udemy/lesson5.java
@@ -1,9 +1,20 @@
 package com.udemy;
 //005 Урок 5 Создание объектов. Понятие «конструктор». Объявление и вызов методов
 public class lesson5 {
-    int sum(int a, int b, int c){
+    int sum(int a, int b, int c){  //параметры
         int result = a + b +c;
         String s = "hello";
         return result;
+    }
+}
+
+
+class testLesson5{
+    public static void main(String[] args){
+        lesson5 t = new lesson5();
+        t.sum(1,2,3);
+        int sum3 = t.sum(1,2,3);
+        System.out.println(sum3);
+        System.out.println(t.sum(3,5,2)); // аргументы функции
     }
 }
