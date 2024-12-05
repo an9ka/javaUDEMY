2be2a0973fd467aed1a5aaf3723f7886a9f4edbe
an9ka <you@example.com>
2024-12-05 23:46:08 +0300

    создание метода

--- src/com/udemy/lesson5.java
+++ src/com/udemy/lesson5.java
@@ -1,4 +1,9 @@
 package com.udemy;
-
+//005 Урок 5 Создание объектов. Понятие «конструктор». Объявление и вызов методов
 public class lesson5 {
+    int sum(int a, int b, int c){
+        int result = a + b +c;
+        String s = "hello";
+        return result;
+    }
 }
