cdda2ddee2278aa3cf7318cd5398f70ca4936115
an9ka <you@example.com>
2024-12-08 15:35:24 +0300

    конструктор

--- src/com/udemy/lesson4Package/Car.java
+++ src/com/udemy/lesson4Package/Car.java
@@ -1,20 +1,22 @@
 package com.udemy.lesson4Package;
 
 public class Car {
-    Car(String cvet, String motor){  //Создание конструктора с параметрами
+   /* Car(String cvet, String motor){  //Создание конструктора с параметрами
     color = cvet;
-    engine = motor;
+    engine = motor; */
+    Car(String cvet, String motor){
+        color = cvet;
+        engine = motor;
+        System.out.println("Цвет машины: " + color + ", Мотор машины: " + engine);
     }
 
     String color;
     String engine;
-    String name = "Anna";
-   // String name = new String("Tetst");
 }
 
 class CarTest {
     public static void main(String[] args){
-        Car car1 = new Car("red", "v5");
-        System.out.println(car1.color + " " + car1.engine);
+        Car car1 = new Car("red", "v3");
+        Car car2 = new Car("white", "v9");
     }
 }
